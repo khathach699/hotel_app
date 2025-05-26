c61a453b58d30dfbd2942634ce5d9fc5b164dc72
KhaThach <khathach699@gmail.com>
2025-05-26 09:32:18 +0700

    fect: set up  structure folder

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+class AppColors {
+  // Primary Theme Colors
+  static const Color primaryColor = Color(0xff2761CE);
+  static const Color secondaryColor = Color(0xff60A5FA);
+  static const Color backgroundColor = Color(0xFFFFFFFF);
+  static const Color cardColor = Color(0xFFF5F5F5);
+
+  // Text Colors
+  static const Color textPrimary = Color(0xFF000000);
+  static const Color textSecondary = Color(0xFF757575);
+  static const Color textOnPrimary = Color(0xFFFFFFFF);
+
+  // Status Colors
+  static const Color successColor = Color(0xFF4CAF50); // Green
+  static const Color infoColor = Color(0xFF2196F3); // Blue
+  static const Color warningColor = Color(0xFFFFC107); // Amber
+  static const Color errorColor = Color(0xFFF44336); // Red
+}
